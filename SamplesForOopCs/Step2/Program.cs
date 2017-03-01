88a79a5dfd260ad3e29a5badf2a3670248bc37b8
AMANO Masaru <github@amapyon.org>
2017-03-02 00:44:05 +0900

    Step2追加

--- SamplesForOopCs/Step2/Program.cs
+++ SamplesForOopCs/Step2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 
-namespace Step1
+namespace Step2
 {
 
     class Program
@@ -11,7 +11,6 @@ namespace Step1
         private static readonly int INITIAL_CALL_UNIT_PRICE = 20;
         private static readonly int FAMILY_SERVICE_BASIC_CHARGE = 100;
         private static readonly string FAMILY_SERVICE_CODE = "C1";
-        private static readonly int FAMILY_SERVICE_TEL_NUMBER = 2;
         private static readonly int DAY_SERVICE_BASIC_CHARGE = 200;
         private static readonly string DAY_SERVICE_CODE = "E1";
         private static readonly int DAY_SERVICE_START_TIME = 8;
@@ -43,14 +42,8 @@ namespace Step1
             var writer = new StreamWriter(fos);
             writer.NewLine = "\n";
 
-            string ownerTelNumber = null;
-            int basicCharge = 0;
-            int callCharge = 0;
-            bool dayServiceJoined = false;
-            bool familyServiceJoined = false;
-            string[] familyTelNumbers = new string[FAMILY_SERVICE_TEL_NUMBER];
-            int familyTelNumberCount = 0;
-            int unitPrice = 0;
+            var values = new Values();
+            values.Clear();
 
             string line = reader.ReadLine();
             while (line != null)
@@ -60,7 +53,7 @@ namespace Step1
                 if (line.StartsWith(RC_OWNER_INFO))
                 {
                     // 契約者情報
-                    ownerTelNumber = line.Substring(RI_OF_OWNER_TEL_NUMBER);
+                    values.ownerTelNumber = line.Substring(RI_OF_OWNER_TEL_NUMBER);
                 }
                 else if (line.StartsWith(RC_SERVICE_INFO))
                 {
@@ -69,13 +62,13 @@ namespace Step1
                     if (DAY_SERVICE_CODE == line.Substring(RI_OF_SERVICE_CODE, RI_SZ_SERVICE_CODE))
                     {
                         // 昼トク割引
-                        dayServiceJoined = true;
+                        values.dayServiceJoined = true;
                     }
                     else if (FAMILY_SERVICE_CODE == line.Substring(RI_OF_SERVICE_CODE, RI_SZ_SERVICE_CODE))
                     {
                         // 家族割引 登録されている電話番号を一時保管
-                        familyServiceJoined = true;
-                        familyTelNumbers[familyTelNumberCount++] = line.Substring(RI_OF_SERVICE_OPTION);
+                        values.familyServiceJoined = true;
+                        values.familyServiceTelNumbers[values.familyServiceTelNumberCount++] = line.Substring(RI_OF_SERVICE_OPTION);
                     }
                 }
                 else if (line.StartsWith(RC_CALL_LOG))
@@ -83,8 +76,8 @@ namespace Step1
                     // 通話記録
 
                     // 単価を計算する
-                    unitPrice = INITIAL_CALL_UNIT_PRICE;
-                    if (dayServiceJoined)
+                    int unitPrice = INITIAL_CALL_UNIT_PRICE;
+                    if (values.dayServiceJoined)
                     {
                         int hour = int.Parse(line.Substring(RI_OF_CALL_START_TIME, RI_SZ_HOUR));
                         if ((hour >= DAY_SERVICE_START_TIME) && (hour <= DAY_SERVICE_END_TIME))
@@ -93,46 +86,43 @@ namespace Step1
                             unitPrice -= 5;
                         }
                     }
-                    for (int i = 0; i < familyTelNumberCount; i++)
+                    for (int i = 0; i < values.familyServiceTelNumberCount; i++)
                     {
-                        if (familyTelNumbers[i] == line.Substring(RI_OF_CALL_NUMBER))
+                        if (values.familyServiceTelNumbers[i] == line.Substring(RI_OF_CALL_NUMBER))
                         {
                             // 家族割引なら半額
                             unitPrice /= 2;
                             break;
                         }
                     }
+
                     // 1通話あたりの通話料を計算し、全通話料に加算する
                     string minutes = line.Substring(RI_OF_CALL_MINUTE, RI_SZ_CALL_MINUTE);
-                    callCharge += unitPrice * int.Parse(minutes);
+                    values.callCharge += unitPrice * int.Parse(minutes);
                 }
                 else if (line.StartsWith(RC_SEPARATOR))
                 {
                     // 区切り
 
                     // 基本料金の計算
-                    basicCharge = INITIAL_BASIC_CHARGE;
-                    if (dayServiceJoined)
+                    int basicCharge = INITIAL_BASIC_CHARGE;
+                    if (values.dayServiceJoined)
                     {
                         basicCharge += DAY_SERVICE_BASIC_CHARGE;
                     }
-                    if (familyServiceJoined)
+                    if (values.familyServiceJoined)
                     {
                         basicCharge += FAMILY_SERVICE_BASIC_CHARGE;
                     }
 
                     // 集計結果の出力
-                    writer.WriteLine("1 " + ownerTelNumber);
+                    writer.WriteLine("1 " + values.ownerTelNumber);
                     writer.WriteLine("5 " + basicCharge);
-                    writer.WriteLine("7 " + callCharge);
+                    writer.WriteLine("7 " + values.callCharge);
                     writer.WriteLine("9 ====================");
 
                     // 変数の初期化
-                    ownerTelNumber = null;
-                    dayServiceJoined = false;
-                    familyServiceJoined = false;
-                    familyTelNumberCount = 0;
-                    callCharge = 0;
+                    values.Clear();
                 }
 
 
---
+++ SamplesForOopCs/Step2/Values.cs
@@ -0,0 +1,31 @@
+﻿namespace Step2
+{
+    class Values
+    {
+        private static readonly int FAMILY_SERVICE_TEL_NUMBER = 2;
+
+        // 契約者電話番号
+        public string ownerTelNumber = null;
+
+        // 通話料金
+        public int callCharge = 0;
+
+        // 昼トク割引
+        public bool dayServiceJoined = false;
+
+        // 家族割引
+        public bool familyServiceJoined = false;
+        public string[] familyServiceTelNumbers = new string[FAMILY_SERVICE_TEL_NUMBER];
+        public int familyServiceTelNumberCount = 0;
+
+        // 変数を初期化する
+        public void Clear()
+        {
+            dayServiceJoined = false;
+            ownerTelNumber = null;
+            familyServiceJoined = false;
+            familyServiceTelNumberCount = 0;
+            callCharge = 0;
+        }
+    }
+}
