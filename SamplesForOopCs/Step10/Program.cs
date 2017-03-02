6e1d86bbd60f323ffd3f23a38409b700f29423ed
AMANO Masaru <github@amapyon.org>
2017-03-03 00:17:20 +0900

    Step10追加

---
+++ SamplesForOopCs/Step10/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+
+namespace Step10
+{
+    public class Program
+    {
+        // 各レコードの先頭文字 (Record Code)
+        private static readonly string RC_OWNER_INFO = "1";
+        private static readonly string RC_SERVICE_INFO = "2";
+        private static readonly string RC_CALL_LOG = "5";
+        private static readonly string RC_SEPARATOR = "9";
+
+        // 各レコードの情報 (Record Information)
+        private static readonly int RI_OF_OWNER_TEL_NUMBER = 2;
+        private static readonly int RI_OF_CALL_MINUTE = 19;
+        private static readonly int RI_SZ_CALL_MINUTE = 3;
+
+        public static void Main(string[] args)
+        {
+
+            var fis = new FileStream("../../../record.log", FileMode.Open);
+            var reader = new StreamReader(fis);
+
+            var fos = new FileStream("../../../invoice.dat", FileMode.Create);
+            var writer = new StreamWriter(fos);
+            writer.NewLine = "\n";
+
+            var invoice = new Invoice();
+            var dayService = new DayService();
+            var familyService = new FamilyService();
+            var services = new ServiceList();
+
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+
+                if (line.StartsWith(RC_OWNER_INFO))
+                {
+                    // 契約者情報
+                    invoice.SetOwnerTelNumber(line.Substring(RI_OF_OWNER_TEL_NUMBER));
+                }
+                else if (line.StartsWith(RC_SERVICE_INFO))
+                {
+                    // 加入サービス情報
+                    services.CheckService(line);
+                }
+                else if (line.StartsWith(RC_CALL_LOG))
+                {
+                    // 通話記録
+
+                    // 単価を計算する
+                    int unitPrice = services.CalcUnitPrice(line);
+
+                    // 1通話あたりの通話料を計算する
+                    int callCharge = unitPrice * int.Parse(line.Substring(RI_OF_CALL_MINUTE, RI_SZ_CALL_MINUTE));
+
+                    // 1通話あたりの通話料を全通話料に加算する
+                    invoice.AddCallCharge(callCharge);
+                }
+                else if (line.StartsWith(RC_SEPARATOR))
+                {
+                    // 区切り
+
+                    // 基本料金の計算
+                    int basicCharge = services.CalcBasicCharge();
+                    invoice.SetBasicCharge(basicCharge);
+
+                    Write(writer, invoice);
+
+                    // 変数の初期化
+                    invoice.Clear();
+                    services.Clear();
+                }
+
+                line = reader.ReadLine();
+            }
+
+            writer.Close();
+            fos.Close();
+
+            reader.Close();
+            fis.Close();
+        }
+
+        private static void Write(StreamWriter writer, Invoice invoice)
+        {
+            // 集計結果の出力
+            writer.WriteLine("1 " + invoice.GetOwnerTelNumber());
+            writer.WriteLine("5 " + invoice.GetBasicCharge());
+            writer.WriteLine("7 " + invoice.GetCallCharge());
+            writer.WriteLine("9 ====================");
+        }
+    }
+}
---
+++ SamplesForOopCs/Step10/ServiceList.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Step10
+{
+    public class ServiceList
+    {
+        // 料金計算のための基礎情報
+        private static readonly int INITIAL_BASIC_CHARGE = 1000;
+        private static readonly int INITIAL_CALL_UNIT_PRICE = 20;
+
+        private List<IService> _services = new List<IService>(){ new DayService(), new FamilyService()};
+
+        public int CalcBasicCharge()
+        {
+            int basicCharge = INITIAL_BASIC_CHARGE;
+            foreach(var service in _services)
+            {
+                basicCharge = service.CalcBasicCharge(basicCharge);
+            }
+            return basicCharge;
+        }
+
+        public int CalcUnitPrice(string line)
+        {
+            double unitPrice = INITIAL_CALL_UNIT_PRICE;
+            foreach (var service in _services)
+            {
+                unitPrice = service.CalcUnitPrice(line, unitPrice);
+            }
+            return (int)unitPrice;
+        }
+
+        public void CheckService(string line)
+        {
+            foreach (var service in _services)
+            {
+                service.CheckService(line);
+            }
+        }
+
+        public void Clear()
+        {
+            foreach(var service in _services)
+            {
+                service.Clear();
+            }
+        }
+    }
+}
