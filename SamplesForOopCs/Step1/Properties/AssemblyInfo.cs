cc026d7b212a8f81876fa012fae562cebcb39ea0
AMANO Masaru <github@amapyon.org>
2017-03-02 23:36:39 +0900

    Step1->Step01

--- SamplesForOopCs/Step1/Properties/AssemblyInfo.cs
+++ SamplesForOopCs/Step1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Step1")]
+[assembly: AssemblyTitle("Step01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Step1")]
+[assembly: AssemblyProduct("Step01")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
