fb1f048ed519c9df4cc5a6f7699ca2ba1ca04241
AMANO Masaru <github@amapyon.org>
2017-03-03 03:21:43 +0900

    Step14追加 readerとwriterのリソース管理にusingを使用。

---
+++ SamplesForOopCs/Step14/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Step14
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            using (var reader = new RecordReader())
+            using (var writer = new InvoiceWriter())
+            {
+                var invoice = new Invoice();
+                var services = new ServiceList();
+
+                for (Record record = reader.ReadRecord(); !record.EOR(); record = reader.ReadRecord())
+                {
+                    Console.WriteLine(record);
+
+                    if (record.IsOwnerRecord())
+                    {
+                        // 契約者情報
+                        invoice.SetOwnerTelNumber(record.OwnerTelNumber());
+                    }
+                    else if (record.IsServiceInfoRecord())
+                    {
+                        // 加入サービス情報
+                        services.CheckService(record);
+                    }
+                    else if (record.IsCallRecord())
+                    {
+                        // 通話記録
+
+                        // 単価を計算する
+                        int unitPrice = services.CalcUnitPrice(record);
+
+                        // 1通話あたりの通話料を計算する
+                        int callCharge = unitPrice * record.CalingTime();
+
+                        // 1通話あたりの通話料を全通話料に加算する
+                        invoice.AddCallCharge(callCharge);
+                    }
+                    else if (record.IsSeparateRecord())
+                    {
+                        // 区切り
+
+                        // 基本料金の計算
+                        int basicCharge = services.CalcBasicCharge();
+                        invoice.SetBasicCharge(basicCharge);
+
+                        writer.Write(invoice);
+
+                        // 変数の初期化
+                        invoice.Clear();
+                        services.Clear();
+                    }
+                }
+            }
+        }
+
+    }
+}
