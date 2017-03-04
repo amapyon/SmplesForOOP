e2c3824410ec549b6a61fbbddc2d9e1c67a23532
AMANO Masaru <github@amapyon.org>
2017-03-04 09:01:01 +0900

    Step15追加 InvoiceWriterTestを追加し、Moqでファイル出力をテスト。

---
+++ SamplesForOopCs/Step15/Record.cs
@@ -0,0 +1,92 @@
+﻿namespace Step15
+{
+    public class Record
+    {
+        // 各レコードの先頭文字 (Record Code)
+        private static readonly string RC_OWNER_INFO = "1";
+        private static readonly string RC_SERVICE_INFO = "2";
+        private static readonly string RC_CALL_LOG = "5";
+        private static readonly string RC_SEPARATOR = "9";
+
+        // 各レコードの情報 (Record Information)
+        private static readonly int RI_OF_OWNER_TEL_NUMBER = 2;
+        private static readonly int RI_OF_SERVICE_CODE = 2;
+        private static readonly int RI_SZ_SERVICE_CODE = 2;
+        private static readonly int RI_OF_SERVICE_OPTION = 5;
+
+        private static readonly int RI_OF_CALL_START_TIME = 13;
+        private static readonly int RI_SZ_HOUR = 2;
+        private static readonly int RI_OF_CALL_MINUTE = 19;
+        private static readonly int RI_SZ_CALL_MINUTE = 3;
+        private static readonly int RI_OF_CALL_NUMBER = 23;
+
+
+        private string _line;
+
+        public Record(string line)
+        {
+            _line = line;
+        }
+
+        public override string ToString()
+        {
+            return _line;
+        }
+
+        internal bool EOR()
+        {
+            return _line == null;
+        }
+
+        public bool IsOwnerRecord()
+        {
+            return _line.StartsWith(RC_OWNER_INFO);
+        }
+
+        public string OwnerTelNumber()
+        {
+            return _line.Substring(RI_OF_OWNER_TEL_NUMBER);
+        }
+
+        public bool IsServiceInfoRecord()
+        {
+            return _line.StartsWith(RC_SERVICE_INFO);
+        }
+
+        public string GetServiceCode()
+        {
+            return _line.Substring(RI_OF_SERVICE_CODE, RI_SZ_SERVICE_CODE);
+        }
+
+        public string GetServiceOption()
+        {
+            return _line.Substring(RI_OF_SERVICE_OPTION);
+        }
+
+        public bool IsCallRecord()
+        {
+            return _line.StartsWith(RC_CALL_LOG);
+        }
+
+        public int GetCallStartHour()
+        {
+            return int.Parse(_line.Substring(RI_OF_CALL_START_TIME, RI_SZ_HOUR));
+        }
+
+        public string GetCallNumber()
+        {
+            return _line.Substring(RI_OF_CALL_NUMBER);
+        }
+
+        public bool IsSeparateRecord()
+        {
+            return _line.StartsWith(RC_SEPARATOR);
+        }
+
+        public int CalingTime()
+        {
+            return int.Parse(_line.Substring(RI_OF_CALL_MINUTE, RI_SZ_CALL_MINUTE));
+        }
+
+    }
+}
---
+++ SamplesForOopCs/Step15/RecordReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Step15
+{
+    public class RecordReader : IDisposable
+    {
+
+        StreamReader _reader = null;
+
+        public RecordReader()
+        {
+            var fis = new FileStream("../../../record.log", FileMode.Open);
+            _reader = new StreamReader(fis);
+        }
+
+        public Record ReadRecord()
+        {
+            return new Record(_reader.ReadLine());
+        }
+
+        public void Close()
+        {
+            _reader.Close(); ;
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+    }
+}
